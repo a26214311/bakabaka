b6e250204adfa141a2497b74a94aeb24b51a6eb5
Flandre <flantan233@gmail.com>
2016-11-10 14:03:40 +0800

    config

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ var app = express();
 var superagent = require('superagent');
 var fs= require('fs');
 var path = require('path');
+const opn = require('opn');
 
 app.listen('8088', function(){
     console.log('server started');
     console.log('http://localhost:8088');
 });
 
-
-//{'Content-Type': 'image/png', 'Cache-Control': 'no-cache'}
-
-var img={};
-
-function loadfiles(imgname){
-	
-}
+opn('http://localhost:8088');
 
 app.get('/getorder', function(req, res) {
 		var querydata = req.query;
@@ -39,36 +33,6 @@ app.get('/getorder', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-	var data = fs.readFileSync('index.html','utf-8');  
+	var data = fs.readFileSync('index.html','utf-8');
 	res.send(data);
-});
-
-app.get('/test', function(req, res) {
-	
-	
-	res.set({
-		  'Content-Type': 'image/png',
-		  'Cache-Control': 'no-cache'
-		});
-    superagent.get('https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png')
-    .end(function(err, sres){
-    	fs.writeFile("3",sres.body);
-    	res.send(sres.body);
-    });
-	
-    
-
-
-
-
-
-
-
-//    sreq.on('end', function(err, sres){
-//
-//    });
-//    sreq.pipe(res);
-
-
-
 });
